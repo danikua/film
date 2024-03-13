083d70d6881471ca7fbf0066e9423980677ef9ff
danikua <daniilbokach@gmail.com>
2024-03-13 16:38:42 +0300

    send all files to git

---
+++ film-catalogue/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+//Components
+import Header from './components/Header';
+import { Home } from './components/Home';
+//Styles
+import { GlobalStyle } from './globalStyle';
+
+const App = () => {
+  return (
+    <div className="App">
+      <Header />
+      <Home />
+      <GlobalStyle />
+    </div>
+  );
+}
+
+export default App;
