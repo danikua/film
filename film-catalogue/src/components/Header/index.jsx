32ddfcd2cadd188cfbf3e2486fe984ab57884dc8
danikua <daniilbokach@gmail.com>
2024-03-23 21:23:20 +0300

    import router

--- film-catalogue/src/components/Header/index.jsx
+++ film-catalogue/src/components/Header/index.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom/dist/umd/react-router-dom.development";
 
 import Logo from '../../images/react-movie-logo.svg';
 import TMDBLogo from '../../images/tmdb_logo.svg';
@@ -8,7 +9,9 @@ import { Wrapper, Content, LogoImg, TMDBLogoImg } from './Header.styles';
 const Header = () => {
  return <Wrapper>
           <Content>
-            <LogoImg src={Logo} alt='logo' />
+            <Link to='/'>
+              <LogoImg src={Logo} alt='logo' />
+            </Link>
             <TMDBLogoImg src={TMDBLogo} alt='tmdb-logo' />
           </Content>
         </Wrapper>
--- film-catalogue/src/components/Thumb/index.jsx
+++ film-catalogue/src/components/Thumb/index.jsx
@@ -1,9 +1,16 @@
 import React from "react";
+import { Link } from "react-router-dom/dist/umd/react-router-dom.development";
 
 import { Image } from "./Thumb.styles";
 
 export const Thumb = ({image, movieId, clickable}) => {
   return <>
+          {clickable ? (
+            <Link to={`/${movieId}`}>
+              <Image src={image} alt='movie-thumb'/>
+            </Link>
+          ) : (
            <Image src={image} alt='movie-thumb'/>
+          )}
          </>
 };
