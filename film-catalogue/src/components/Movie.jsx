8f732e9e5c3a1429cef319f4faa835e49db1c64b
danikua <daniilbokach@gmail.com>
2024-03-25 22:01:52 +0300

    created useMovieFetch hook and started creating Movie page

--- film-catalogue/src/components/Movie.jsx
+++ film-catalogue/src/components/Movie.jsx
@@ -1,3 +1,26 @@
 import React from "react";
+import { useParams } from "react-router-dom/dist/umd/react-router-dom.development";
+
+import { IMAGE_BASE_URL, POSTER__SIZE } from "../config";
+
+import { BreadCrumb } from "./BreadCrumb";
+import { FilmsGrid } from "./FilmsGrid";
+import Spinner from "./Spinner";
+
+import { useMovieFetch } from "../Hooks/useMovieFetch";
+
+import noImg from '../images/no_image.jpg';
  
-export const Movie = () => <div>Movie</div>;
+export const Movie = () => {
+  const {movieId} = useParams();
+
+  const {state: movie, loading, error} = useMovieFetch(movieId);
+
+  if (loading) return <Spinner />
+  if (error) return <div>Something went wrong...</div>
+  return (
+    <>
+      <BreadCrumb movieTitle={movie.original_title} />
+    </>
+  )
+};
